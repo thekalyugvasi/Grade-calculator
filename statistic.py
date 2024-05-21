f43f2c86aa89bbbfd9278eee8e58ada5a4af1c0e
Saurabh Kesarwani <126169600+thekalyugvasi@users.noreply.github.com>
2024-05-21 22:03:40 +0530

    Add files via upload

---
+++ statistic.py
@@ -0,0 +1,19 @@
+import statistics
+
+# Sample list of numbers
+data = []
+Num=int(input("enter a array:"))
+while Num = 7:
+    i=7;
+
+# Calculate the mean (average)
+mean = statistics.mean(data)
+print("Mean:", mean)
+
+# Calculate the median (middle value)
+median = statistics.median(data)
+print("Median:", median)
+
+# Calculate the mode (most common value)
+mode = statistics.mode(data)
+print("Mode:", mode)
